4685cc49d1d83fe84587e3539ce77bae358ea2db
tedyagure <agure84ngochi@gmail.com>
2020-08-15 17:56:17 +0530

    add upload

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const redis = require("redis");
-
+const multer = require('multer');
+const inMemoryStorage = multer.memoryStorage();
+const uploadStrategy = multer({storage: inMemoryStorage}).single('image');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -19,10 +21,6 @@ const client = redis.createClient({
 const usersRedisKey = "store:users"; // cahce key for users
 const dataExpireTime = 3600; // 1 hour cache expire time
 
-// main endpoint
-app.get("/", (req, res) =>
-  res.send("Welcome to Node.js + redis boilerplate API.")
-);
 
 // users endpoint with caching
 app.get("/users", (req, res) => {
@@ -50,6 +48,35 @@ app.get("/users", (req, res) => {
         });
     }
   });
+});
+
+app.post("/file/upload", uploadStrategy, (req, res) => {
+  try {
+    axios
+      .post("https://tedyfunc.azurewebsites.net/api/HttpTrigger1", {
+        filedata: req.file.buffer,
+        filename: req.file.originalname,
+      })
+      .then((Res) => {
+        if (Res.status === 200) {
+          return res.status(200).json({
+            message: "Image Uploaded!",
+            statusCode: 200,
+          });
+        } else {
+          throw error;
+        }
+      })
+      .catch((e) => {
+        return res.status(200).json({
+          message: "Image Upload failed!",
+          statusCode: 400,
+        });
+        console.error(e);
+      });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 // user details endpoint
